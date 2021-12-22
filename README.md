593dddc4885daedc43af82ea4c388d1bf1938c70
Dekatron322 <muritalaibrahim097@gmail.com>
2021-12-22 05:23:15 -0800

    iotx, gft, vita, ioshib, meta, zoom, WoW added

---
+++ README.md
@@ -0,0 +1,44 @@
+# IoTexChart
+
+charts for iotex tokens | realtime
+
+This App provides information of all tokens on the iotex blockain:
+real time price
+market cap
+total supply
+price chart and more.
+
+
+
+## Demo
+Visit https://iotexchartapp.com/ . The demo is updated whenever the demo branch code is updated.
+
+
+## Usage
+It's best to install Python projects in a Virtual Environment. Once you have set up a VE, clone this project
+
+```bash
+git clone https://github.com/Dekatron322/IotexChart
+```
+Then
+
+```bash
+cd IotexChart
+```
+Run
+
+```python
+python manage.py migrate # run first migration
+python manage.py runserver # run the server
+```
+Then locate http://172.0.0.1:8000
+
+
+
+## Roadmap
+To build a fully fledged chart platform for all tokens on the IoTex blockchain.
+
+
+
+
+
---
+++ main/urls.py
@@ -0,0 +1,36 @@
+from django.urls import path
+from . import views
+
+app_name = "main"
+
+urlpatterns = [
+
+	path("", views.IndexView, name="index"),
+
+	path("banner/", views.BannerView, name="banner"),
+
+	path("unvetted/", views.unvetted, name="unvetted"),
+	path('verify/banner/<int:pk>/', views.VerifyBannerView, name="verify_banner"),
+	path('verify/vetted/<int:pk>/', views.VerifyVettedView, name="verify_vetted"),
+
+	path("signup/", views.signup, name='signup'),
+    path("signin/", views.signin, name='signin'),
+	path('all-banner/', views.AllBannerView, name="all_banner"),
+
+	path('all-vetted/', views.AllVettedView, name="all_vetted"),
+
+
+	#coin urls 
+	path("staking/", views.StakingView, name="staking"),
+	path("your-stake/", views.YourStakeView, name="your_stake"),
+	path("imagictoken/", views.ImagicTokenView, name="imagictoken"),
+	path("metanyx/", views.MetanyxView, name="metanyx"),
+	path("iotex-shiba/", views.IotexShibaView, name="iotex_shiba"),
+	path("iotex/", views.IotexView, name="iotex"),
+	path("zoom-swap/", views.ZoomSwapView, name="zoom_swap"),
+	path("vitality/", views.VitalityView, name="vitality"),
+	path("wow/", views.WowSwapView, name="wow"),
+	path("game-fantasy-token/", views.GameFantasyTokenView, name="game_fantasy_token"),
+
+]
+
